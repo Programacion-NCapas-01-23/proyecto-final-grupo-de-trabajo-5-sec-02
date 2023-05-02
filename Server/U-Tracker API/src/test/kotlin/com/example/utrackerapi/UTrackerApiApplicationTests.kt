5646016fa75c634d50474276ab0f7e39523915cb
Dgarzona16 <0027119@uca.edu.sv>
2023-05-02 14:45:46 -0600

    add project templates

---
+++ Server/U-Tracker API/src/test/kotlin/com/example/utrackerapi/UTrackerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.utrackerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UTrackerApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
