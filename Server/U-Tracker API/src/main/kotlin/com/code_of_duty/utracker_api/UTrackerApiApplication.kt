d7c7ceb0fa6efbe756e96e66e0c1c5d10a2b7703
Dgarzona16 <0027119@uca.edu.sv>
2023-05-04 08:13:40 -0600

    :construction: add principal entities

---
+++ Server/U-Tracker API/src/main/kotlin/com/code_of_duty/utracker_api/UTrackerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.code_of_duty.utracker_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UTrackerApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<UTrackerApiApplication>(*args)
+}
---
+++ Server/U-Tracker API/src/test/kotlin/com/code_of_duty/utracker_api/UTrackerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.code_of_duty.utracker_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UTrackerApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
