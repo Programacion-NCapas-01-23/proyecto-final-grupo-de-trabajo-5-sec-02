eac71e13f04175820545d5bb5f118340f90a36c1
Axwell17 <00145319@uca.edu.sv>
2023-06-12 21:49:16 -0600

    :hammer: Modificacion endpoint de Materias

---
+++ Server/U-Tracker API/src/main/kotlin/com/code_of_duty/utracker_api/data/dtos/AssesmentDto.kt
@@ -0,0 +1,9 @@
+package com.code_of_duty.utracker_api.data.dtos
+
+data class AssesmentDto (
+    val name: String = "",
+    val percentage: Int = 0,
+    val date: String = "",
+    val grade: Int = 0,
+
+)
