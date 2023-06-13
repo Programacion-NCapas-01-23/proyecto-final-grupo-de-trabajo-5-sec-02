19f6ced8572d81d300a2b808c30e51ea586cfa81
Edwin Leiva <ed5leiva@gmail.com>
2023-06-13 16:29:48 -0600

    ⚡ creating the Career Redux flow

--- Client/Website/src/state/store.ts
+++ Client/Website/src/state/store.ts
@@ -1,9 +1,11 @@
 import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit'
 import facultyReducer from './slices/facultySlice'
+import careerReducer from './slices/careerSlice'
 
 const store =  configureStore({
     reducer: {
         faculty: facultyReducer,
+        career: careerReducer,
     }
 })
 
