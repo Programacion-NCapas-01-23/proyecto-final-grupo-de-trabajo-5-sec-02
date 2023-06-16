bfddcfbf466a024ee13cc55491294efbba356980
Edwin Leiva <ed5leiva@gmail.com>
2023-06-15 23:39:30 -0600

    👽 mapping the api

--- Client/Website/src/api/routes.ts
+++ Client/Website/src/api/routes.ts
@@ -1,18 +1,51 @@
 import axios, {AxiosInstance} from "axios";
 
-export const baseURL: string = 'http://localhost:8080';
+export const baseURL: string = 'http://localhost:8080/admin';
 
 export const api: AxiosInstance = axios.create({
     baseURL
 });
 
 export const routes = {
+    auth: {
+        login: `${baseURL}/login`,
+        register: `${baseURL}/Register`,
+    },
+    classTime: {
+        add: `${baseURL}/class-time/add`,
+        update: `${baseURL}/class-time/update`,
+        delete: `${baseURL}/class-time/delete`,
+    },
+    cycle: {
+        add: `${baseURL}/cycles/add`,
+        delete: `${baseURL}/cycles/delete`,
+        update: `${baseURL}/cycles/update`,
+    },
+    degrees: {
+        add: `${baseURL}/degrees/add`,
+        delete: `${baseURL}/degrees/delete`,
+        update: `${baseURL}/degrees/update`,
+    },
     faculties: {
-        newFaculty: `${baseURL}/faculties/add`,
-        deleteFaculty: `${baseURL}/faculties/delete`,
-        getAllFaculties: `${baseURL}/faculties`,
+        add: `${baseURL}/faculties/add`,
+        delete: `${baseURL}/faculties/delete`,
+        update: `${baseURL}/faculties/update`,
     },
-    careers: {
-        newCareer: `${baseURL}/`,
+    pensum: {
+        add: `${baseURL}/pensum/add`,
+        delete: `${baseURL}/pensum/delete`,
+        update: `${baseURL}/pensum/update`,
     },
+    schedules: {
+        add: `${baseURL}/schedules/add`,
+        delete: `${baseURL}/schedules/delete`,
+        deleteAll: `${baseURL}/schedules/delete/all`,
+        update: `${baseURL}/schedules/update`,
+    },
+    subject: {
+        add: `${baseURL}/subjects/add`,
+        delete: `${baseURL}/subjects/delete`,
+        deleteAll: `${baseURL}/subjects/delete/all`,
+        update: `${baseURL}/subjects/update`,
+    }
 }
