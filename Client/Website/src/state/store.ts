c3d59d28e28912090ffb90851bf3c3f3fd8cac3c
Edwin Leiva <ed5leiva@gmail.com>
2023-06-22 20:18:40 -0600

    ⚡ adding the reducers to the store

--- Client/Website/src/state/store.ts
+++ Client/Website/src/state/store.ts
@@ -1,11 +1,21 @@
 import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit'
 import facultyReducer from './slices/facultySlice'
 import careerReducer from './slices/careerSlice'
+import classTimeReducer from './slices/classTimeSlice'
+import cycleReducer from './slices/cycleSlice'
+import pensumReducer from './slices/pensumSlice'
+import scheduleReducer from './slices/scheduleSlice'
+import subjectReducer from './slices/subjectSlice'
 
 const store =  configureStore({
     reducer: {
         faculty: facultyReducer,
         career: careerReducer,
+        classTime: classTimeReducer,
+        cycle: cycleReducer,
+        pensum: pensumReducer,
+        schedule: scheduleReducer,
+        subject: subjectReducer,
     }
 })
 
