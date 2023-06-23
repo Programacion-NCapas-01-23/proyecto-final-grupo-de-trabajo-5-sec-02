9167ae9360b7e6a4430c3c1f6b09d70491da74ac
Dgarzona <00271419@uca.edu.sv>
2023-06-23 16:36:21 -0600

    add repos and daos with RoomModule

---
+++ Client/App/app/src/main/java/com/code_of_duty/u_tracker/data/database/UtrackerDatabase.kt
@@ -0,0 +1,23 @@
+package com.code_of_duty.u_tracker.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.code_of_duty.u_tracker.data.database.dao.TokenDao
+import com.code_of_duty.u_tracker.data.database.dao.UserDao
+import com.code_of_duty.u_tracker.data.database.entities.User
+import com.code_of_duty.u_tracker.data.database.entities.UserToken
+
+@Database(
+    entities = [
+        User::class,
+        UserToken::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class UtrackerDatabase : RoomDatabase(){
+    abstract fun userDao(): UserDao
+    abstract fun userTokenDao(): TokenDao
+}
