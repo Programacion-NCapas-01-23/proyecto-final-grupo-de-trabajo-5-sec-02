6e7148724d5f18c8153f8d93524705cc6710cac0
Edwin Leiva <ed5leiva@gmail.com>
2023-06-22 20:20:15 -0600

    ⚡ updating the RootState

--- Client/Website/src/state/initialState.ts
+++ Client/Website/src/state/initialState.ts
@@ -1,8 +1,18 @@
 import Faculty from "@/interfaces/Faculty";
 import InitialState from "@/interfaces/InitialState";
 import Career from "@/interfaces/Career";
+import ClassTime from "@/interfaces/ClassTime";
+import Cycle from "@/interfaces/Cycle";
+import Pensum from "@/interfaces/Pensum";
+import Schedule from "@/interfaces/Schedule";
+import Subject from "@/interfaces/Subject";
 
 export type RootState = {
     faculty: InitialState<Faculty>;
-    career: InitialState<Career>
+    career: InitialState<Career>;
+    classTime: InitialState<ClassTime>;
+    cycle: InitialState<Cycle>;
+    pensum: InitialState<Pensum>;
+    schedule: InitialState<Schedule>;
+    subject: InitialState<Subject>;
 }
